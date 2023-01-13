a4f98cf8f46156e88f8a606fda63d42fa20b6a03
YoonDii <dbswl9506@naver.com>
2023-01-13 17:51:18 +0900

    add admin

--- 2회차/photo/models.py
+++ 2회차/photo/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Photo(models.Model):
-    title : models.CharField(max_length=50)
-    author : models.CharField(max_length=50)
-    image : models.CharField(max_length=200)
-    description : models.TextField()
-    price : models.IntegerField()
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    image = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.IntegerField()
